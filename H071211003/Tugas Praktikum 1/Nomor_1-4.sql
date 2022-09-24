7c20e2a53d22b0e4e15e9ea4d0a89e0a609d8c1b
selvianiamalia <selviaml2020@gmail.com>
2022-09-24 10:19:22 +0800

    Uploud Tugas

---
+++ H071211003/Tugas Praktikum 1/Nomor_1-4.sql
@@ -0,0 +1,16 @@
+-- USE offices;
+-- CREATE TABLE offices (
+-- officeCode VARCHAR (10) NOT NULL,
+-- city VARCHAR (50) NOT NULL,
+-- phone VARCHAR (50) NOT NULL,
+-- addressline1 VARCHAR (50) NOT NULL,
+-- addressline2 VARCHAR (50),
+-- state VARCHAR (50),
+-- country VARCHAR (50) NOT NULL,
+-- PRIMARY KEY (officeCode)
+-- );
+-- DESC offices;
+
+-- ALTER TABLE offices MODIFY COLUMN phone INT (20) NOT NULL;
+-- ALTER TABLE offices DROP COLUMN addressline2;
+-- DESC offices;
