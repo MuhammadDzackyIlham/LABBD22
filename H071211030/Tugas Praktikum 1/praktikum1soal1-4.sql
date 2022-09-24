49b61679f354829b9e9adc6b4006aa9439107f87
Ramstuy <ikrhidayat@gmail.com>
2022-09-24 16:48:25 +0800

    Upload Tugas Praktikum 1

---
+++ H071211030/Tugas Praktikum 1/praktikum1soal1-4.sql
@@ -0,0 +1,25 @@
+#CREATE DATABASE office;
+-- USE office;
+
+-- Soal 1
+#CREATE TABLE offices(
+-- officeCode VARCHAR (10) NOT NULL,
+--  city VARCHAR (50) NOT NULL,
+-- phone VARCHAR (50) NOT NULL,
+-- addressline1 VARCHAR(50) NOT NULL,
+-- addressline2 VARCHAR (50),
+-- state VARCHAR (50),
+-- country VARCHAR (50) NOT NULL,
+-- PRIMARY KEY (officeCode)
+-- );
+
+-- Soal 2
+-- DESCRIBE offices;
+
+-- Soal 3
+-- ALTER TABLE offices 
+-- MODIFY phone INT(20);
+
+-- Soal 4
+-- ALTER TABLE offices
+-- DROP addressline2;
