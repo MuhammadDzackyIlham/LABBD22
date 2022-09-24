a79385eb8c42e7e50b521ff1e88f5b6323e752ff
selvianiamalia <114160377+selvianiamalia@users.noreply.github.com>
2022-09-24 14:39:48 +0800

    Update Nomor_1-4.sql

--- H071211003/Tugas Praktikum 1/Nomor_1-4.sql
+++ H071211003/Tugas Praktikum 1/Nomor_1-4.sql
@@ -1,22 +1,22 @@
 -- USE offices;
 -- Nomor 1
--- CREATE TABLE offices (
--- officeCode VARCHAR (10) NOT NULL,
--- city VARCHAR (50) NOT NULL,
--- phone VARCHAR (50) NOT NULL,
--- addressline1 VARCHAR (50) NOT NULL,
--- addressline2 VARCHAR (50),
--- state VARCHAR (50),
--- country VARCHAR (50) NOT NULL,
--- PRIMARY KEY (officeCode)
--- );
+CREATE TABLE offices (
+officeCode VARCHAR (10) NOT NULL,
+city VARCHAR (50) NOT NULL,
+phone VARCHAR (50) NOT NULL,
+addressline1 VARCHAR (50) NOT NULL,
+addressline2 VARCHAR (50),
+state VARCHAR (50),
+country VARCHAR (50) NOT NULL,
+PRIMARY KEY (officeCode)
+);
 
 -- Nomor 2
--- DESC offices;
+DESC offices;
 
 -- Nomor 3
--- ALTER TABLE offices MODIFY COLUMN phone INT (20) NOT NULL;
+ALTER TABLE offices MODIFY COLUMN phone INT (20) NOT NULL;
 
 -- Nomor 4
--- ALTER TABLE offices DROP COLUMN addressline2;
--- DESC offices;
+ALTER TABLE offices DROP COLUMN addressline2;
+DESC offices;
