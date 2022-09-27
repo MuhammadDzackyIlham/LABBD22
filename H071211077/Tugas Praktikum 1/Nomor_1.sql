02c8f38039b8bffd047b2ac3fe0c6e7c2f5cb306
RifkyAqid10 <muhrifkyaqid@gmail.com>
2022-09-27 18:25:38 +0800

    Tugas Praktikum 1

---
+++ H071211077/Tugas Praktikum 1/Nomor_1.sql
@@ -0,0 +1,25 @@
+-- CREATE DATABASE Tugas1;
+
+-- USE Tugas1;
+
+-- create table offices(
+-- officeCode VARCHAR(10) NOT NULL,
+-- city VARCHAR(50) NOT NULL,
+-- phone VARCHAR(50) NOT NULL,
+-- addressline1 VARCHAR(50) NOT NULL,
+-- addressline2 VARCHAR(50),
+-- state VARCHAR(50),
+-- country VARCHAR(50) NOT NULL,
+-- PRIMARY KEY(officeCode)
+
+
+-- );
+
+-- DESC offices;
+-- ALTER TABLE offices
+-- MODIFY phone INT(20);
+
+
+
+-- ALTER TABLE offices
+-- DROP addressline2;
